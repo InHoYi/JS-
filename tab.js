9e058f80ad839068e898fc954d0d5135aebcff58
InHoYi <inhomike17@gmail.com>
2025-01-14 22:07:52 +0900

    반복문으로 수정

--- tab.js
+++ tab.js
@@ -1,27 +1,3 @@
-// $('.tab-button').eq(0).on('click', function() {
-//     $('.tab-button').removeClass('orange');
-//     $('.tab-button').eq(0).addClass('orange');
-//     $('.tab-content').removeClass('show');
-//     $('.tab-content').eq(0).addClass('show');
-// })
-
-
-// $('.tab-button').eq(1).on('click', function() {
-//     $('.tab-button').removeClass('orange');
-//     $('.tab-button').eq(1).addClass('orange');
-//     $('.tab-content').removeClass('show');
-//     $('.tab-content').eq(1).addClass('show');
-// })
-
-
-// $('.tab-button').eq(2).on('click', function() {
-//     $('.tab-button').removeClass('orange');
-//     $('.tab-button').eq(2).addClass('orange');
-//     $('.tab-content').removeClass('show');
-//     $('.tab-content').eq(2).addClass('show');
-// })
-
-
 for (let i = 0; i < 3; i++){
     $('.tab-button').eq(i).on('click', function() {
         $('.tab-button').removeClass('orange');
