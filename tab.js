52b980322b42064d8ad73f34a2129fbfbac089ee
InHoYi <inhomike17@gmail.com>
2025-01-15 11:57:10 +0900

    이벤트 버블링 방법 +1

--- tab.js
+++ tab.js
@@ -11,10 +11,19 @@ function openTab(tabNumber) {
     $('.tab-content').eq(tabNumber).addClass('show');
 }
 
-for (let i = 0; i < 3; i++){
-    $('.list').on('click', function(e) {
-        if (e.target == document.querySelectorAll('.tab-button')[i]) {
-            openTab(i);
-        }
-    })
-}
+// 이벤트 버블링 1번 방법
+// for (let i = 0; i < 3; i++){
+//     $('.list').on('click', function(e) {
+//         if (e.target == document.querySelectorAll('.tab-button')[i]) {
+//             openTab(i);
+//         }
+//     })
+// }
+
+// 이벤트 버블링 2번 방법
+$('.list').on('click', function(e) {
+    // .list만 클릭해도 .list 하위 클래스를 모두 같이 클릭을 당함.
+    openTab(e.target.dataset.id);
+    // html 태그에 저장한 데이터를 불러오는 방법
+    console.log(e.target.dataset.id);
+})
