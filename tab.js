ee861e1d1c67de52cef591dcd92da5b360c4d711
InHoYi <inhomike17@gmail.com>
2025-01-15 11:39:50 +0900

    openTab() 함수로 만듬

--- tab.js
+++ tab.js
@@ -1,8 +1,20 @@
+// for (let i = 0; i < 3; i++){
+//     $('.tab-button').eq(i).on('click', function() {
+//         openTab(i);
+//     })  
+// }
+
+function openTab(tabNumber) {
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(tabNumber).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(tabNumber).addClass('show');
+}
+
 for (let i = 0; i < 3; i++){
-    $('.tab-button').eq(i).on('click', function() {
-        $('.tab-button').removeClass('orange');
-        $('.tab-button').eq(i).addClass('orange');
-        $('.tab-content').removeClass('show');
-        $('.tab-content').eq(i).addClass('show');
-    })  
+    $('.list').on('click', function(e) {
+        if (e.target == document.querySelectorAll('.tab-button')[i]) {
+            openTab(i);
+        }
+    })
 }
